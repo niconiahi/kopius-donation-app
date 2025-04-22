192217619374e23f4e15524dcc6bc2f51a4cbc23
Yael Pilar <yaelpilarluque@gmail.com>
2025-04-22 20:30:49 +0000

    UI

--- app/routes/playground.tsx
+++ app/routes/playground.tsx
@@ -1,14 +1,14 @@
-///YAE
 import {
   Form,
   useActionData,
   useLoaderData,
   type ActionFunctionArgs,
-  type LinksFunction
+  type LinksFunction,
 } from "react-router";
 
 import * as v from "valibot";
-
+import styles from "src/styles/navbar.css?url";
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export const NameSchema = v.pipe(v.string(), v.minLength(5));
 export const DonationSchema = v.object({
@@ -62,40 +62,52 @@ export default function () {
   const { donations } = useLoaderData<typeof loader>();
   const actionData = useActionData<typeof action>();
   return (
-  <>
-    <div className="navbar"
-       style={{
-        backgroundColor: '#f8f9fa', 
-        padding: '1rem', 
-        display: 'flex', 
-        alignItems: 'center',
-      }}
-    >
-      <img src="./assets/kopius-star.png" alt="Logo Kopius" 
-      style={{
-          height: '40px',
-        }}/>
-    </div>
-    <div className="information">
-      <h1>Fundacion Si</h1>
-    <img src="./assets/fundacion-si.png" alt="Logo Fundacion Si" />
-    <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat et, consectetur inventore consequuntur adipisci fugiat commodi aut nulla esse ducimus. Sint, quo sunt! Ad saepe tempora maxime sit praesentium porro!</h2>
-    </div>
-    <main>
-      <ul>
-        {donations.map((donation) => {
-          return <li key={`donation-${donation.id}`}>{donation.id}</li>;
-        })}
-      </ul>
-      <Form method="POST">
-        <input type="hidden" name="action" value={ACTION.CREATE_DONATION} />
-        <input name="name" />
-        <button type="submit">Create donation</button>
-      </Form>
-      {actionData ? (
-        <span>Just created the donation {actionData.name}</span>
-      ) : null}
-    </main>
-      </>
-    );
+    <>
+      <nav className="navbar">
+        <img
+          className="logo-kopius"
+          src="./assets/symbol_gradient.png"
+          alt="Logo Kopius"
+        />
+      </nav>
+      <div className="information">
+        <div className="header">
+          <img src="./assets/logo-small.png" alt="Logo Fundacion Si" />
+          <h1>Fundación Si</h1>
+        </div>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+          inventore suscipit necessitatibus explicabo accusantium alias nesciunt
+          blanditiis quidem ratione nihil, quaerat aut delectus vitae eius,
+          eaque tempore debitis iusto consectetur.
+        </h2>
+      </div>
+      <div className="causes">
+        <a href="/pagina1" className="cause-item">
+          <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+        </a>
+        <a href="/pagina2" className="cause-item">
+          <h3>Causa ......</h3>
+        </a>
+        <a href="/pagina3" className="cause-item">
+          <h3>Causa....</h3>
+        </a>
+      </div>
+      <main>
+        <ul>
+          {donations.map((donation) => {
+            return <li key={`donation-${donation.id}`}>{donation.id}</li>;
+          })}
+        </ul>
+        <Form method="POST">
+          <input type="hidden" name="action" value={ACTION.CREATE_DONATION} />
+          <input name="name" />
+          <button type="submit">Create donation</button>
+        </Form>
+        {actionData ? (
+          <span>Just created the donation {actionData.name}</span>
+        ) : null}
+      </main>
+    </>
+  );
 }
